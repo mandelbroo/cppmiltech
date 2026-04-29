58b789008a1eda1356b611fd09aeb03b6b8f4c68
mandelbroo <vitalii.makarets@gmail.com>
2026-04-29 19:16:58 +0000

    review and cleanup

--- homework_04/src/main.cpp
+++ homework_04/src/main.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-const int TICKS_PER_REVOLUTION = 1024;  // 1024 імпульсів на оберт колеса
-const float WHEEL_RADIUS_M = 0.3;       // 30 см
-const float WHEELBASE_M = 1.0;          // 1 м відстань між передньою та задньою віссю
+const float TICKS_PER_REVOLUTION = 1024.0;  // 1024 імпульсів на оберт колеса
+const float WHEEL_RADIUS_M = 0.3;           // 30 см
+const float WHEELBASE_M = 1.0;              // 1 м відстань між передньою та задньою віссю
 const string OUTPUT_FILE_PATH = "output.txt";
 
 int main(int argc, char** argv) {
@@ -18,54 +18,51 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  ifstream inputFile(argv[1], ios::in);
+  fstream inputFile(argv[1], ios::in);
 
   if (!inputFile.is_open()) {
     cerr << "Error: Could not open the file:" << argv[1] << endl;
     return 1;
   }
 
-  std::ofstream outputFile(OUTPUT_FILE_PATH, ios::out);
+  fstream outputFile(OUTPUT_FILE_PATH, ios::out);
 
   if (!outputFile.is_open()) {
-    cout << "Error. Cannot open output file." << endl;
+    cerr << "Error: Could not open the output file:" << OUTPUT_FILE_PATH << endl;
     return 1;
   }
 
+  int timestamp_ms;
+  bool isFirstStep = true;
   float x = 0, y = 0, theta = 0;
-  int timestamp_ms, fl_ticks, fr_ticks, bl_ticks, br_ticks;
-  int prev_fl_ticks, prev_fr_ticks, prev_bl_ticks, prev_br_ticks;
-  int index = 0;
+  float fl_ticks, fr_ticks, bl_ticks, br_ticks;
+  float prev_fl_ticks, prev_fr_ticks, prev_bl_ticks, prev_br_ticks;
   string line;
 
   while (getline(inputFile, line)) {
     istringstream ss(line);
     ss >> timestamp_ms >> fl_ticks >> fr_ticks >> bl_ticks >> br_ticks;
 
-    if (index == 0) {
-      index++;
+    if (isFirstStep) {
+      isFirstStep = false;
       prev_fl_ticks = fl_ticks;
       prev_fr_ticks = fr_ticks;
       prev_bl_ticks = bl_ticks;
       prev_br_ticks = br_ticks;
       outputFile << timestamp_ms << " " << x << " " << y << " " << theta << endl;
-      continue;  // Пропускаємо перший елемент оскільки нам потрібен попередній зразок для обчислення різниць
+
+      continue;  // Пропускаємо перший елемент оскільки нам потрібен два елементи для обчислення дельти
     }
 
     // Крок 1. Delta iмпульсiв по кожному колесу:
-    int d_fl = fl_ticks - prev_fl_ticks;
-    int d_fr = fr_ticks - prev_fr_ticks;
-    int d_bl = bl_ticks - prev_bl_ticks;
-    int d_br = br_ticks - prev_br_ticks;
-
-    prev_fl_ticks = fl_ticks;
-    prev_fr_ticks = fr_ticks;
-    prev_bl_ticks = bl_ticks;
-    prev_br_ticks = br_ticks;
+    float d_fl = fl_ticks - prev_fl_ticks;
+    float d_fr = fr_ticks - prev_fr_ticks;
+    float d_bl = bl_ticks - prev_bl_ticks;
+    float d_br = br_ticks - prev_br_ticks;
 
     // Крок 2. Усереднити борти (передне i заднє колесо одного боку обертаються синхронно):
-    int d_left = (d_fl + d_bl) / 2;
-    int d_right = (d_fr + d_br) / 2;
+    float d_left = (d_fl + d_bl) / 2.0;
+    float d_right = (d_fr + d_br) / 2.0;
 
     // Крок 3. Перевести iмпульси у метри:
     float distance_per_tick = 2 * M_PI * WHEEL_RADIUS_M / TICKS_PER_REVOLUTION;
@@ -73,25 +70,25 @@ int main(int argc, char** argv) {
     float dR = d_right * distance_per_tick;
 
     // Крок 4. Скiльки пройшов центр робота i на скiльки повернувся:
-    float d = (dL + dR) / 2;                 // пройдена вiдстань центру
+    float d = (dL + dR) / 2.0;               // пройдена вiдстань центру
     float dtheta = (dR - dL) / WHEELBASE_M;  // змiна орiєнтацiї
 
     // Крок 5. Оновити позицiю (midpoint integration - усереднений напрямок на кроцi):
-    x += d * cos(theta + dtheta / 2);
-    y += d * sin(theta + dtheta / 2);
+    x += d * cos(theta + dtheta / 2.0);
+    y += d * sin(theta + dtheta / 2.0);
     theta += dtheta;
 
-    outputFile << timestamp_ms << " " << x << " " << y << " " << theta << endl;
-
-    index++;
     prev_fl_ticks = fl_ticks;
     prev_fr_ticks = fr_ticks;
     prev_bl_ticks = bl_ticks;
     prev_br_ticks = br_ticks;
+    outputFile << timestamp_ms << " " << x << " " << y << " " << theta << endl;
   }
 
   inputFile.close();
   outputFile.close();
+
+  cout << "Odometry calculation completed. Output written to: " << OUTPUT_FILE_PATH << endl;
 
   return 0;
 }
